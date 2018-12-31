24a8863feeaea93223d85897d9af04c628dc2664
zoma4 <45948124+zoma4@users.noreply.github.com>
2018-12-31 15:36:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -351,7 +351,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers`);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1play | Last Music`,"http://twitch.tv/Death Shop")
+client.user.setGame(`Type +play and enjoy`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
